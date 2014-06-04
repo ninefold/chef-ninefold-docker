4b6e2d232b7a1736de6706228005cc232b3ddb7e
Warren Bain <warren@ninefold.com>
2014-06-04 13:35:20 +1000

    Add user to docker group and add default images

---
+++ attributes/default.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook Name:: ninefold_docker
+# Attributes:: default
+#
+# Copyright (C) 2014 Ninefold Pty Limited
+#
+# All rights reserved - Do Not Redistribute
+#
+
+default['ninefold_docker']['group_members'] = %w(user)
+
+default['ninefold_docker']['images'] = [
+  { :name => 'dockerfile/elasticsearch' },
+  {
+    :name => 'tutum/mysql',
+    :tag  => '5.5'
+  },
+  { :name => 'dockerfile/redis' },
+  { :name => 'coreos/etcd' }
+]
--- recipes/default.rb
+++ recipes/default.rb
@@ -9,4 +9,19 @@
 # Set up a server with docker
 #
 
+# set up users
+node.set['docker']['group_members'] = node['ninefold_docker']['group_members']
+
+# set up docker
 include_recipe 'docker'
+
+# pull in any containers, will update with latest available
+# use :pull_if_missing for one time pull from registry
+# TODO: uses default docker registry - specify location of image?
+
+node['ninefold_docker']['images'].each do |image|
+  docker_image image[:name] do
+    tag image[:tag] if image[:tag]
+    action :pull
+  end
+end

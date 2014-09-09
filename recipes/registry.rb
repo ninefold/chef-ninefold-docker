689ef03f28932aaea54bb91451ddfc1e2f8a2b08
Warren Bain <warren@ninefold.com>
2014-09-09 11:38:18 +1000

    Add apt recipe to update package lists

--- recipes/registry.rb
+++ recipes/registry.rb
@@ -7,11 +7,12 @@
 # All rights reserved - Do Not Redistribute
 #
 
+include_recipe 'apt'
+
 # requires a 64 character random string as a secret key
 # so generate one if necessary
 
 require 'securerandom'
-
 node.set_unless['docker-registry']['secret_key'] = SecureRandom.hex(32)
 
 include_recipe 'docker-registry::application_server'
